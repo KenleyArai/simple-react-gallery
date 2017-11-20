c9526e003ef8afef5a8fcda68d328a2184930ec5
Kenley Arai <me@kenleyarai.com>
2017-11-19 20:39:20 -0800

    setup sanitize.css

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,7 +2,11 @@ import React, { Component } from "react";
 
 class App extends Component {
   render() {
-    return <div className="App">Hello</div>;
+    return (
+      <div className="App">
+        <h1>dasd</h1>
+      </div>
+    );
   }
 }
 
