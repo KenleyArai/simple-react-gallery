f5155fde8fb90ef77ac95243432df52e1c131f16
Kenley Arai <me@kenleyarai.com>
2017-11-25 10:46:49 -0800

    Added back button to single photos

--- src/components/Bottombar.jsx
+++ src/components/Bottombar.jsx
@@ -1,6 +1,10 @@
 import React, { PureComponent } from "react";
 import PropTypes from "prop-types";
 
+import Bar from "components/Bar";
+import BarImg from "components/BarImg";
+import BarButton from "components/BarButton";
+
 export default class BottomBar extends PureComponent {
   static propTypes = {
     url: PropTypes.string.isRequired,
@@ -9,8 +13,80 @@ export default class BottomBar extends PureComponent {
     position: PropTypes.number.isRequired
   };
 
+  prev_helper(count, position, photoHandler) {
+    if (position > 1) {
+      return (
+        <BarButton onClick={() => photoHandler(position - 1)}>Prev</BarButton>
+      );
+    }
+  }
+
+  next_helper(count, position, photoHandler) {
+    if (position != count) {
+      return (
+        <BarButton onClick={() => photoHandler(position + 1)}>Next</BarButton>
+      );
+    }
+  }
+
+  bar_helper(url, count, position, photoHandler) {
+    // 3 basic states
+    // At the beginning
+    // While in the middle
+    // At the end
+
+    if (position === 1) {
+      return (
+        <Bar>
+          <BarImg
+            onClick={() => photoHandler(position + 1)}
+            srcSet={url + (position + 1) + ".jpg"}
+          />
+          <BarImg
+            onClick={() => photoHandler(position + 2)}
+            srcSet={url + (position + 2) + ".jpg"}
+          />
+        </Bar>
+      );
+    } else if (count - position === 1) {
+      return (
+        <Bar>
+          <BarImg
+            onClick={() => photoHandler(position - 2)}
+            srcSet={url + (position - 2) + ".jpg"}
+          />
+          <BarImg
+            onClick={() => photoHandler(position - 1)}
+            srcSet={url + (position - 1) + ".jpg"}
+          />
+        </Bar>
+      );
+    } else {
+      return (
+        <Bar>
+          <BarImg
+            onClick={() => photoHandler(position - 1)}
+            srcSet={url + (position - 1) + ".jpg"}
+          />
+          <BarImg srcSet={url + position + ".jpg"} />
+          <BarImg
+            onClick={() => photoHandler(position + 1)}
+            srcSet={url + (position + 1) + ".jpg"}
+          />
+        </Bar>
+      );
+    }
+  }
+
   render() {
     var { url, count, photoHandler, position } = { ...this.props };
-    return <div />;
+
+    return (
+      <Bar>
+        {this.prev_helper(count, position, photoHandler)}
+        {this.bar_helper(url, count, position, photoHandler)}
+        {this.next_helper(count, position, photoHandler)}
+      </Bar>
+    );
   }
 }
