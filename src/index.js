72fe97759040946a3f925a5dd1b6432fe4f9684e
Kenley Arai <me@kenleyarai.com>
2017-11-27 13:36:12 -0800

    cleaned up how the images are hosted

--- src/index.js
+++ src/index.js
@@ -8,28 +8,37 @@ import registerServiceWorker from "./registerServiceWorker";
 var robocar_payload = {
   title: "Robocar",
   desc: "Remote control car",
-  url:
-    "https://kenley.nyc3.digitaloceanspaces.com/personal-website/desktop/robocar/slideshow/",
+  url: "https://kenley.nyc3.digitaloceanspaces.com/images/robocar/",
   count: 9
 };
 
-var cdc_payload = {
-  title: "Research at the CDC",
+var sensor_payload = {
+  title: "Mosquito Classifier",
   desc: "My trip at the cdc",
-  url:
-    "https://kenley.nyc3.digitaloceanspaces.com/personal-website/desktop/cdc/slideshow/",
-  count: 10
+  url: "https://kenley.nyc3.digitaloceanspaces.com/images/sensor/",
+  count: 28
 };
 
 var wedding_payload = {
   title: "Wedding website",
   desc: "Ruby on rails 5 api",
   link: "http://lena.kenleyar.ai/",
-  url:
-    "https://kenley.nyc3.digitaloceanspaces.com/personal-website/desktop/cdc/slideshow/1.jpg"
+  url: "https://kenley.nyc3.digitaloceanspaces.com/images/wedding/0.jpg"
 };
 
-var album_payloads = [robocar_payload, cdc_payload, wedding_payload];
+var serial_payload = {
+  title: "Serial Face",
+  desc: "box that unlocks using image processing",
+  link: "http://lena.kenleyar.ai/",
+  url: "https://kenley.nyc3.digitaloceanspaces.com/images/serial-face/0.jpg"
+};
+
+var album_payloads = [
+  robocar_payload,
+  sensor_payload,
+  wedding_payload,
+  serial_payload
+];
 
 ReactDOM.render(
   <Album title={"test gallery"} galleries={album_payloads} />,
