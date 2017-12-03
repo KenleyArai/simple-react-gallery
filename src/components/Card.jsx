69d14f6aa5a5bf83f043202cd0cb4d01278570f0
Kenley Arai <me@kenleyarai.com>
2017-12-03 15:37:06 -0800

    Added button to Card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,22 +5,29 @@ import Box from "components/Box";
 import LazyImg from "components/LazyImg";
 import Header from "components/Header";
 import Content from "components/Content";
+import Button from "containers/Button";
 
 export default class Card extends Component {
   static propTypes = {
     url: PropTypes.string,
     desc: PropTypes.string,
     title: PropTypes.string,
-    link: PropTypes.string
+    link: PropTypes.string,
+    clickHandler: PropTypes.func
   };
 
   render() {
-    var { url, desc, title, link } = { ...this.props };
+    var { url, desc, title, link, clickHandler } = { ...this.props };
     return (
       <Box>
         <LazyImg srcSet={url} />
-        <Header>{title}</Header>
-        <Content>{desc}</Content>
+        <Box>
+          <Header>{title}</Header>
+          <Content>{desc}</Content>
+          <Box>
+            <Button clickHandler={clickHandler} />
+          </Box>
+        </Box>
       </Box>
     );
   }
