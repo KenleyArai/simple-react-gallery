34679154993c36cf32d200c64c3a53d468aca162
Kenley Arai <me@kenleyarai.com>
2017-12-02 18:13:39 -0800

    first test passed

--- src/tests/Bar.test.js
+++ src/tests/Bar.test.js
@@ -1,14 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { shallow } from "enzyme";
-import Bar from "components/Bar";
 import { configure } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
+import renderer from "react-test-renderer";
+import "jest-styled-components";
 
 configure({ adapter: new Adapter() });
+
+import Bar from "components/Bar";
 
 describe("Bar", () => {
   it("should render bar", () => {
     const wrapper = shallow(<Bar />);
+    expect(wrapper).toMatchSnapshot();
   });
 });
