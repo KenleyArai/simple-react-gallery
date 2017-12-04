cca5792fe7b72f3dac7f1ecff1a32253c0c0d1d6
Kenley Arai <me@kenleyarai.com>
2017-12-03 16:48:50 -0800

    Finished LinkedIn Badge

--- src/styled/BadgeStyle.jsx
+++ src/styled/BadgeStyle.jsx
@@ -1,9 +1,12 @@
 import styled from "styled-components";
 
 import blank from "../images/blank.svg";
+import linkedin from "../images/linkedin.svg";
 
 function background_helper(type) {
   switch (type) {
+    case "linkedin":
+      return linkedin;
     case "blank":
       return blank;
     default:
