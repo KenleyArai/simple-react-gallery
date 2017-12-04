9f9a0717f3588f55532376d2744e1104c7b879d3
Kenley Arai <me@kenleyarai.com>
2017-12-03 16:43:42 -0800

    Passed test for blank background_helper

--- src/styled/BadgeStyle.jsx
+++ src/styled/BadgeStyle.jsx
@@ -1,5 +1,18 @@
 import styled from "styled-components";
 
+import blank from "../images/blank.svg";
+
+function background_helper(type) {
+  switch (type) {
+    case "blank":
+      return blank;
+    default:
+      return blank;
+  }
+  return blank;
+}
+
 const BadgeStyle = styled.div``;
 
+export { background_helper };
 export default BadgeStyle;
